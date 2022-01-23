d74108a6d02f13c2fbcdca14424188f1b1ed4728
Xuân Hoàng <xhoang0509@gmail.com>
2022-01-23 10:06:12 +0700

    update file SQL

--- tai lieu phan tich/abcshop.sql
+++ tai lieu phan tich/abcshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 14, 2022 at 11:26 AM
+-- Generation Time: Jan 23, 2022 at 04:04 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.0.13
 
@@ -67,11 +67,12 @@ CREATE TABLE `customers` (
 --
 
 INSERT INTO `customers` (`id`, `name`, `email`, `password`, `photo`, `address`, `phone`, `token`) VALUES
-(1, 'Nguyễn Xuân Hoàng', 'xhoang0509@gmail.com', '1', '1642084851.png', '																																																						tam ***- yên ***- bắc ***\r\n											\r\n											\r\n											\r\n											\r\n											\r\n											\r\n											\r\n											\r\n					', '0857812113', 'user_61e038be47eca2.633353801642084542'),
+(1, 'Nguyễn Xuân Hoàng', 'xhoang0509@gmail.com', '1', '1642813322.jpg', 'khong co dia chi 123																																																																																												', '0857812113', 'user_61ebb9e9398564.017788371642838505'),
 (11, 'Nguyễn Xuân Hoàng', 'xhoang05091@gmail.com', '1', '1640404667.png', '', '', ''),
 (14, '', '', '', '', '', '', ''),
 (15, 'Hoang Nguyen Xuan', 'xhoang05092@gmail.com', '1', '', '', '', ''),
-(17, 'Tran Huy Tien', 'tien@gmail.com', '1', '1642127195.jpg', 'bac giang								\r\n											\r\n											\r\n						\r\n					', '0123 456 789', 'user_61e0d37d7ccc73.524096891642124157');
+(17, 'Tran Huy Tien', 'tien@gmail.com', '1', '1642127195.jpg', 'bac giang								\r\n											\r\n											\r\n						\r\n					', '0123 456 789', 'user_61e0d37d7ccc73.524096891642124157'),
+(18, 'test', 'x@gmail.com', '1', '', 'viet nam', '012', '');
 
 -- --------------------------------------------------------
 
@@ -110,7 +111,7 @@ CREATE TABLE `orders` (
   `notes` text NOT NULL,
   `status` int(11) NOT NULL,
   `total_price` float NOT NULL,
-  `created_at` date NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -118,12 +119,32 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `customer_id`, `name_receiver`, `phone_receiver`, `address_receiver`, `notes`, `status`, `total_price`, `created_at`) VALUES
-(3, 17, 'Tran Huy Tien', '0678', '																																																																																																																																																																																																\r\n		bac giang									\r\n											\r\n											\r\n											\r\n', '', 0, 400000, '2022-01-14'),
-(4, 17, 'Tran Huy Tien', '0678', '																																																																																																																																																																																																\r\n		bac giang									\r\n											\r\n											\r\n											\r\n', '', 0, 400000, '2022-01-14'),
-(5, 17, 'Tran Huy Tien', '0678', '																																																																																																																																																																																																\r\n		bac giang									\r\n											\r\n											\r\n											\r\n', '', 0, 400000, '2022-01-14'),
-(6, 17, 'Tran Huy Tien', '0678', '																																																																																																																																																																																																\r\n		bac giang									\r\n											\r\n											\r\n											\r\n', '', 0, 400000, '2022-01-14'),
-(7, 17, 'Tran Huy Tien', '0678', '																																																																																																																																																																																																\r\n		bac giang									\r\n											\r\n											\r\n											\r\n', '', 0, 400000, '2022-01-14'),
-(8, 17, 'Tran Huy Tien', '0123 456 789', 'bac giang									\r\n											\r\n											\r\n											\r\n						\r\n					', '', 0, 5400000, '2022-01-14');
+(3, 17, 'Tran Huy Tien', '0678', '																																																																																																																																																																																																\r\n		bac giang									\r\n											\r\n											\r\n											\r\n', '', 1, 400000, '2022-01-13 17:00:00'),
+(4, 17, 'Tran Huy Tien', '0678', '																																																																																																																																																																																																\r\n		bac giang									\r\n											\r\n											\r\n											\r\n', '', 1, 400000, '2022-01-13 17:00:00'),
+(5, 17, 'Tran Huy Tien', '0678', '																																																																																																																																																																																																\r\n		bac giang									\r\n											\r\n											\r\n											\r\n', '', 1, 400000, '2022-01-13 17:00:00'),
+(6, 17, 'Tran Huy Tien', '0678', '																																																																																																																																																																																																\r\n		bac giang									\r\n											\r\n											\r\n											\r\n', '', 1, 400000, '2022-01-13 17:00:00'),
+(7, 17, 'Tran Huy Tien', '0678', '																																																																																																																																																																																																\r\n		bac giang									\r\n											\r\n											\r\n											\r\n', '', -1, 400000, '2022-01-13 17:00:00'),
+(8, 17, 'Tran Huy Tien', '0123 456 789', 'bac giang									\r\n											\r\n											\r\n											\r\n						\r\n					', '', -1, 5400000, '2022-01-13 17:00:00'),
+(9, 1, 'Nguyễn Xuân Hoàng', '0857812113', '																																																						tam ***- yên ***- bắc ***\n											\n											\n											\n											\n											\n											\n											\n											\n					', 'xuan hoang', -1, 0, '2022-01-21 17:00:00'),
+(10, 0, 'Nguyễn Xuân Hoàng', '0857812113', 'khong co dia chi 123																																																																																												', 'aaa', 0, 0, '2022-01-21 17:00:00'),
+(11, 0, 'Nguyễn Xuân Hoàng', '0857812113', 'khong co dia chi 123																																																																																												', '', 0, 0, '2022-01-21 17:00:00'),
+(12, 0, 'Nguyễn Xuân Hoàng', '0857812113', 'khong co dia chi 123																																																																																												', '', 0, 0, '2022-01-21 17:00:00'),
+(13, 1, 'Nguyễn Xuân Hoàng', '0857812113', 'khong co dia chi 123																																																																																												', '', -1, 0, '2022-01-21 17:00:00'),
+(14, 1, 'Nguyễn Xuân Hoàng', '0857812113', 'khong co dia chi 123																																																																																												', '', -1, 2000000, '2022-01-21 17:00:00'),
+(15, 1, 'Nguyễn Xuân Hoàng', '0857812113', 'khong co dia chi 123																																																																																												', '', -1, 2000000, '2022-01-21 17:00:00'),
+(16, 1, 'Nguyễn Xuân Hoàng', '0857812113', 'khong co dia chi 123																																																																																												', '', -1, 1400000, '2022-01-21 17:00:00'),
+(17, 18, 'test', '012', 'xuan hoang', '', -1, 1400000, '2022-01-21 17:00:00'),
+(19, 18, 'test', '012', 'viet nam', '', 1, 1200000, '2022-01-21 17:00:00'),
+(20, 18, 'test', '012', 'viet nam', '', 1, 4400000, '2022-01-21 17:00:00'),
+(26, 1, 'Nguyễn Xuân Hoàng', '0857812113', 'khong co dia chi 123																																																																																												', '', -1, 400000, '2022-01-21 17:00:00'),
+(27, 1, 'Nguyễn Xuân Hoàng', '0857812113', 'khong co dia chi 123																																																																																												', '', -1, 1400000, '2022-01-21 17:00:00'),
+(28, 1, 'Nguyễn Xuân Hoàng', '0857812113', 'khong co dia chi 123																																																																																												', '', -1, 400000, '2022-01-21 17:00:00'),
+(29, 1, 'Nguyễn Xuân Hoàng', '0857812113', 'khong co dia chi 123																																																																																												', '', -1, 4000000, '2022-01-21 17:00:00'),
+(30, 1, 'Nguyễn Xuân Hoàng', '0857812113', 'khong co dia chi 123																																																																																												', '', -1, 1000000, '2022-01-21 17:00:00'),
+(31, 1, 'Nguyễn Xuân Hoàng', '0857812113', 'khong co dia chi 123																																																																																												', '', -1, 400000, '2022-01-21 17:00:00'),
+(32, 1, 'Nguyễn Xuân Hoàng', '0857812113', 'khong co dia chi 123																																																																																												', '', 1, 400000, '2022-01-21 17:00:00'),
+(33, 1, 'Nguyễn Xuân Hoàng', '0857812113', 'khong co dia chi 123																																																																																												', '', 1, 1000000, '2022-01-21 17:00:00'),
+(34, 1, 'Nguyễn Xuân Hoàng', '0857812113', 'khong co dia chi 123																																																																																												', '', 1, 1000000, '2022-01-21 17:00:00');
 
 -- --------------------------------------------------------
 
@@ -147,7 +168,37 @@ INSERT INTO `order_product` (`order_id`, `product_id`, `quantity`) VALUES
 (7, 4, 1),
 (8, 3, 1),
 (8, 4, 1),
-(8, 6, 1);
+(8, 6, 1),
+(9, 3, 4),
+(10, 4, 6),
+(10, 6, 12),
+(11, 6, 1),
+(11, 7, 1),
+(15, 7, 1),
+(15, 6, 1),
+(16, 6, 1),
+(16, 4, 1),
+(17, 6, 1),
+(17, 4, 1),
+(18, 9, 1),
+(19, 4, 4),
+(20, 4, 1),
+(20, 3, 1),
+(21, 7, 1),
+(22, 3, 1),
+(23, 4, 1),
+(24, 4, 1),
+(25, 4, 1),
+(26, 4, 1),
+(27, 4, 1),
+(27, 6, 1),
+(28, 4, 1),
+(29, 3, 1),
+(30, 6, 1),
+(31, 4, 1),
+(32, 4, 1),
+(33, 7, 1),
+(34, 6, 1);
 
 -- --------------------------------------------------------
 
@@ -236,7 +287,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `manufacturers`
@@ -248,7 +299,7 @@ ALTER TABLE `manufacturers`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `products`
